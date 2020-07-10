8371714cec993719551e87686dc64b806527e79d
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2020-07-11 00:18:24 +0200

    add unit test to chceck conversion of data in WeatherForecast

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -5,20 +5,29 @@ namespace test
 {
     public class Tests
     {
-        public Class1 Class { get; private set; }
+        public WeatherForecast WeatherForecast { get; private set; }
 
         [SetUp]
         public void Setup()
         {
-            this.Class = new Class1();
+            this.WeatherForecast = new WeatherForecast();
         }
 
         [Test]
-        public void Test1()
+        public void Should_Create_Instance_Of_WeatherForecast()
         {
-            if(!this.Class.Equals(null)) {
+            if(!this.WeatherForecast.Equals(null)) {
                 Assert.Pass();
             };
         }
+
+        [TestCase(-10, 15)]
+        [TestCase(0, 32)]
+        [TestCase(10, 49)]
+        public void Should_Convert_Temperature_From_C_To_F(int C, int F)
+        {
+            this.WeatherForecast.TemperatureC = C;
+            Assert.AreEqual(this.WeatherForecast.TemperatureF, F);
+        }        
     }
 }
